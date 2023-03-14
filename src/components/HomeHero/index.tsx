c87dc3b07911f8614a0e4d39e779203d2faecb4f
MatheusVict <matheusvictorhenrique@gmail.com>
2023-03-14 15:57:57 -0300

    box of home

--- src/components/HomeHero/index.tsx
+++ src/components/HomeHero/index.tsx
@@ -1,4 +1,4 @@
-import { Container, TextContainer } from './styles';
+import { CodeItem, Container, InfosContainer, TextContainer } from './styles';
 
 export function HomeHero() {
   return (
@@ -9,6 +9,31 @@ export function HomeHero() {
           <h1>Olá</h1>
           <h2>Me chamo Matheus</h2>
         </TextContainer>
+        <InfosContainer>
+          <CodeItem>
+            <span className="comment">// Minha apresentação</span>
+            <span className="purple">Infos </span>
+            {'\u007B'}
+            <div>
+              Nome: <span className="blue">Matheus,</span>
+            </div>
+            <div>
+              Sobrenome: <span className="blue">Victor</span>
+            </div>
+            {'\u007D'}
+          </CodeItem>
+          <CodeItem>
+            <span className="purple">Cargo </span>
+            {'\u007B'}
+            <div>
+              Função: <span className="blue">Dev Full-stack,</span>
+            </div>
+            <div>
+              Empresa: <span className="blue">Google</span>
+            </div>
+            {'\u007D'}
+          </CodeItem>
+        </InfosContainer>
       </div>
     </Container>
   );
